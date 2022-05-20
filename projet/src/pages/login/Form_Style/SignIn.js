68055ca9367fbca1bce1de0bff1c4ddbce9151eb
Souu-m <minnas0302@gmail.com>
2022-05-20 01:05:53 +0100

    logver2

--- projet/src/pages/login/Form_Style/SignIn.js
+++ projet/src/pages/login/Form_Style/SignIn.js
@@ -11,51 +11,55 @@ import LockIcon from "@mui/icons-material/Lock";
 import { border, height, padding } from "@mui/system";
 function SignIn() {
 	return (
-		<div className="login-container">
-			<div className="left-side"></div>
-			<form className="right-side">
-				<TextField
-					id="input-with-icon-textfield"
-					InputProps={{
-						startAdornment: (
-							<InputAdornment position="start">
-								<PersonIcon />
-							</InputAdornment>
-						),
-						placeholder: "Your Name",
-					}}
-					required
-					variant="standard"
-					margin="normal"
-				/>
-				<TextField
-					id="input-with-icon-textfield"
-					InputProps={{
-						startAdornment: (
-							<InputAdornment position="start">
-								<LockIcon />
-							</InputAdornment>
-						),
-						placeholder: "Password",
-					}}
-					variant="standard"
-					margin="normal"
-				/>
-				<Button
-					variant="contained"
-					size="normal"
-					sx={{
-						width: "%80",
-						border: "none",
-						cursor: "pointer",
+		<div className="main-login">
+			<div className="login-container">
+				<div className="left-side"></div>
+				<div className="right-side">
+					<form>
+						<TextField
+							id="input-with-icon-textfield"
+							InputProps={{
+								startAdornment: (
+									<InputAdornment position="start">
+										<PersonIcon />
+									</InputAdornment>
+								),
+								placeholder: "Your Name",
+							}}
+							required
+							variant="standard"
+							margin="normal"
+						/>
+						<TextField
+							id="input-with-icon-textfield"
+							InputProps={{
+								startAdornment: (
+									<InputAdornment position="start">
+										<LockIcon />
+									</InputAdornment>
+								),
+								placeholder: "Password",
+							}}
+							variant="standard"
+							margin="normal"
+						/>
+						<Button
+							variant="contained"
+							size="normal"
+							sx={{
+								width: "%80",
+								border: "none",
+								cursor: "pointer",
 
-						transition: "all 0.3 ease",
-						mt: "1.2rem",
-					}}
-				>
-					<div className="buttontexte">Log in</div>
-				</Button>
-			</form>
+								transition: "all 0.3 ease",
+								mt: "1.2rem",
+							}}
+						>
+							<div className="buttontexte">Log in</div>
+						</Button>
+					</form>
+				</div>
+			</div>
 		</div>
 	);
 }
